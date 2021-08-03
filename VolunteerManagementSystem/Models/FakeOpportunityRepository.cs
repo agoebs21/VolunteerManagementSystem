611cf85bbd67b34f9dffc491e10b56acd93ffa01
Matthew Campbell <87869756+MC2the2@users.noreply.github.com>
2021-08-03 14:09:21 -0400

    Update FakeOpportunityRepository.cs

--- VolunteerManagementSystem/Models/FakeOpportunityRepository.cs
+++ VolunteerManagementSystem/Models/FakeOpportunityRepository.cs
@@ -1,5 +1,15 @@
-new Opportunity { OpportunityID =  "1", Title =  "Science Fair", Description
- =  "A person critiques the projects in the science fair.  He/she also promotes good science education by teaching the parents about the scientific method." },
-new Opportunity { OpportunityID =  "2", Title =  "Health Clinic", Description =  "The volunteer assess the health of any patients that come into the clinic." },
-new Opportunity { OpportunityID =  "3", Title =  "Computer Education", Description =  "A person teaches the young students how to use a computer." },
-new Opportunity { OpportunityID =  "4", Title =  "Product Management", Description =  "The volunteer evaluates the status of ingoing/outgoing deliveries." }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace VolunteerManagementSystem.Models
+{
+ public class FakeVolunteertRepository : IVolunteerRepository
+ {
+ public IEnumerable<Opportunity> Opportunities => new List<Opportunity>{new Opportunity { OpportunityID =  1, Title =  "Science Fair", Description =  "A person critiques the projects in the science fair.  He/she also promotes good science education by teaching the parents about the scientific method." },
+new Opportunity { OpportunityID =  2, Title =  "Health Clinic", Description =  "The volunteer assess the health of any patients that come into the clinic." },
+new Opportunity { OpportunityID =  3, Title =  "Computer Education", Description =  "A person teaches the young students how to use a computer." },
+new Opportunity { OpportunityID =  4, Title =  "Product Management", Description =  "The volunteer evaluates the status of ingoing/outgoing deliveries." }
+};
+ }
+}
