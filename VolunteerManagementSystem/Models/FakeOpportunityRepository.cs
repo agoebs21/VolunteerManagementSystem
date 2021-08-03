f57a7de985b377e51bb15759c3b5a52670335c30
Matthew Campbell <87869756+MC2the2@users.noreply.github.com>
2021-08-03 14:10:21 -0400

    Update FakeOpportunityRepository.cs

--- VolunteerManagementSystem/Models/FakeOpportunityRepository.cs
+++ VolunteerManagementSystem/Models/FakeOpportunityRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 namespace VolunteerManagementSystem.Models
 {
- public class FakeVolunteertRepository : IVolunteerRepository
+ public class FakeOpportunityRepository : IVolunteerRepository
  {
  public IEnumerable<Opportunity> Opportunities => new List<Opportunity>{new Opportunity { OpportunityID =  1, Title =  "Science Fair", Description =  "A person critiques the projects in the science fair.  He/she also promotes good science education by teaching the parents about the scientific method." },
 new Opportunity { OpportunityID =  2, Title =  "Health Clinic", Description =  "The volunteer assess the health of any patients that come into the clinic." },
