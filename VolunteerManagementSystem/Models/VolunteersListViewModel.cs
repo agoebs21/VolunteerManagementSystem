1fb5237a21d7c51ea56f3f7a0d7090d1666cb70f
Matthew Campbell <87869756+MC2the2@users.noreply.github.com>
2021-08-07 17:22:21 -0400

    Update VolunteersListViewModel.cs

--- VolunteerManagementSystem/Models/VolunteersListViewModel.cs
+++ VolunteerManagementSystem/Models/VolunteersListViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SearchFunctionality.Models
+namespace VolunteerManagementSystem.Models
 {
     public class VolunteersListViewModel
     {
