c60e4f934f06654f1707356551e3892737f62d60
Matthew Campbell <87869756+MC2the2@users.noreply.github.com>
2021-08-07 17:19:37 -0400

    Create SearchTerm.cs

---
+++ VolunteerManagementSystem/Models/SearchTerm.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolunteerManagementSystem.Models
+{
+    public class SearchTerm
+    {
+        public string SearchString { get; set; }
+    }
+}
