170080055d972278d50502cee828165acf72c6e1
Matthew Campbell <87869756+MC2the2@users.noreply.github.com>
2021-08-08 19:10:38 -0400

    Update FakeVolunteerRepository.cs

--- VolunteerManagementSystem/Models/FakeVolunteerRepository.cs
+++ VolunteerManagementSystem/Models/FakeVolunteerRepository.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-namespace SearchFunctionality.Models
+namespace VolunteerManagementSystem.Models
 {
     public class FakeVolunteerRepository : IVolunteerRepository
     {
